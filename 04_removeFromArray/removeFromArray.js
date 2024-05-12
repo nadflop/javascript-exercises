e1b42f92a98cde0d1c122bcbec671dfced982872
nad <31370689+nadflop@users.noreply.github.com>
2024-05-12 23:08:41 +0900

    Finished exercise 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...elements) {
+    if (elements.length == 1) return myArray.filter((word) => word != elements);
+    
+    const args = Array.from(elements);
+    const result = myArray.filter((word) => {
+        return !args.includes(word);
+    });
 
+    return result;
 };
 
 // Do not edit below this line
