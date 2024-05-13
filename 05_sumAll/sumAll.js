90e73d683f2b7245dd7bb86cbb1fa0f3fa6a5e2b
nadflop <nadhiraqilah@gmail.com>
2024-05-13 17:30:32 +0900

    Completed exercise 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+    if (num1 < 0 || num2 < 0) return 'ERROR';
+    
+    const array = [num1, num2].sort();
+    var result = 0;
+    for (let i = array[0]; i <= array[1]; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
