5a88600e65dd0860ca1b6e8f484b3046e1f3f2f3
nad <31370689+nadflop@users.noreply.github.com>
2024-05-15 23:09:51 +0900

    finished!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+function getAge(death, birth) {
+    return death ? death - birth : new Date().getFullYear() - birth;
+}
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currPerson) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currAge = getAge(currPerson.yearOfDeath, currPerson.yearOfBirth);
+        return oldestAge < currAge ? currPerson : oldest;
+    });
 };
 
 // Do not edit below this line
