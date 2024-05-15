24f8048d88fe2f07b777b8afb192b6adbe091dd3
nad <31370689+nadflop@users.noreply.github.com>
2024-05-15 21:54:48 +0900

    finished get the titles using .map()

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    var books = Array.from(arr);
+    const title = books.map(book => book.title);
+    return title;
 };
 
 // Do not edit below this line
