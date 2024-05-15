a35ec5fab78507024ec6af83707943f2f47aa1bd
nad <31370689+nadflop@users.noreply.github.com>
2024-05-15 21:35:52 +0900

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    var re = /[\W]/g;
+    var reverseStr = Array.from(str).reverse().join("").toLowerCase().replace(re, '');
+    
+    return (reverseStr === str.toLowerCase().replace(re, ''));
 };
 
 // Do not edit below this line
