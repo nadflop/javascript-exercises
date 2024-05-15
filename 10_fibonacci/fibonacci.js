39b864cd2d84fbd6ef6fb0292e1280d41d468115
nad <31370689+nadflop@users.noreply.github.com>
2024-05-15 21:46:32 +0900

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequence) {
+    if (sequence < 0) return "OOPS";
+    if (sequence == 0) return 0;
+    if (sequence == 1 || sequence == 2) return 1;
+    return fibonacci(sequence-1) + fibonacci(sequence-2);
 };
 
 // Do not edit below this line
