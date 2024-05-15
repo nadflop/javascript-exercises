83ad96a9a519480b57430e87ad542b9ea5e728db
nad <31370689+nadflop@users.noreply.github.com>
2024-05-15 21:11:52 +0900

    complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sumOfAll = array.reduce((total, currenItem) => {
+    return total + currenItem;
+  }, 0);
+
+  return parseInt(sumOfAll);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  const multiplyAll = array.reduce((total, currenItem) => {
+    return total * currenItem;
+  },1);
 
+  return parseInt(multiplyAll);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) return -1;
+  else if (num == 0) return 1;
+  else return (num * factorial(num-1));
 };
 
 // Do not edit below this line
